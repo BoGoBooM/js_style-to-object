024950856cea9121c24db1222ac6cf29e64adc6d
BoGoBooM <myPikcha@gmail.com>
2025-03-20 15:07:10 +0200

    task was solved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  if (!sourceString) {
+    return result;
+  }
+
+  const rules = sourceString.split(';');
+
+  rules.forEach((rule) => {
+    const [key, value] = rule.split(':').map((part) => {
+      const trimmedPart = part.trim();
+
+      return trimmedPart;
+    });
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;
